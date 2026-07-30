e30986dc0d79d923b69ee8bfa4b6185e58cba451
yansen99999999-tech <yansen99999999@gmail.com>
2026-07-30 10:57:47 +0700

    feat:Pertemuan anok blade

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,19 +8,49 @@ class StudentController extends Controller
 {
     public function index()
     {
-        return "Ini adalah halaman daftar siswa";
+        $title = "Sistem Sekolah - Daftar Siswa";
+        $students =[
+        [
+            'id' => 1,
+            'nis' => '1001',
+            'name' => 'Andi',
+            'class' => 'XII TKJ 1',
+            'major' => 'TKJ',
+        ],
+        [
+            'id' => 2,
+            'nis' => '1002',
+            'name' => 'Budi',
+            'class' => 'XII AKL 1',
+            'major' => 'AKL'
+        ]
+        ];
+        return view('students.index', [
+            'title' => $title,
+            'students' => $students
+        ]);
     }
     public function show($id)
     {
-        return "Ini adalah halaman detail siswa dengan id: " . $id;
+         $title = "Sistem Sekolah - Detail Siswa";
+        return view('students.show', [
+            'title' => $title
+        ]);
     }
     public function create()
     {
-        return "Ini adalah halaman tambah siswa";          
+        
+         $title = "Sistem Sekolah - Tambah Siswa";
+        return view('students.create', [
+            'title' => $title
+        ]);          
     }
     public function edit($id)
     {
-        return "Ini adalah halaman edit siswa dengan id: " . $id;
+        $title = "Sistem Sekolah - Edit Siswa";
+        return view('students.edit', [
+            'title' => $title
+        ]);
     }
     public function store(Request $request)
     {
